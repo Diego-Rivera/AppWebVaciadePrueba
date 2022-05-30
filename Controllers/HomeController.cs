2f4b8d5f2951ae349a806200f02858716bc12997
Diego-Rivera <diego.rivera.ramirez@gmail.com>
2022-05-30 11:18:29 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppWebVaciadePrueba.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.NombreUsuario = "Diego Rivera";
+            ViewBag.Title = "Inicio del sitio";
+            int[] edades = { 47, 51, 25, 23 };
+            ViewData["Edad"] =edades;
+            
+            TempData["valor1"] = 80000;
+
+
+            return View();
+
+        }
+
+        public string Saludar(int rut)
+        {
+            return "Bienvendo amigo tu RUT es: " + rut;
+        }
+    }
+}
